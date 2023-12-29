27ba33de1f0c625c0d6ca33cdea9e660123fa266
Davi Mendes <davi26030@gmail.com>
2023-12-28 22:03:54 -0300

    folders realocateds

--- processing/ecommerce/models.py
+++ processing/ecommerce/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# Nome do livro - texto com 200 caracteres + data da publicação
+class Livro(models.Model):
+    nome_livro = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+# Autor - Todo livro vai estar vinculado a um autor
+class Autor(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    nome_autor = models.CharField(max_length = 200)
+    paginas = models.IntegerField(default=0)
