df87759264a7efb4e96d72d4654ceb49213829c6
Davi Mendes <davi26030@gmail.com>
2023-12-29 12:54:55 -0300

    added href link to resumo

--- processing/ecommerce/models.py
+++ processing/ecommerce/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 # Nome do livro - texto com 200 caracteres + data da publicação
 class Autor(models.Model):
     nome = models.CharField(max_length=200)
-    
+
+
 # Autor - Todo livro vai estar vinculado a um autor
 class Livro(models.Model):
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    nome = models.CharField(max_length = 200)
-    paginas = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date published')
+    nome = models.CharField(max_length=200)
+    paginas = models.IntegerField(default=0, blank=True, null=True)
+    resumo = models.TextField(blank=True, null=True)
+    pub_date = models.DateTimeField("date published")
